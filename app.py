d2f374d6b371e1f5e006e689a28ecca27a89462d
B <2319890@cognizant.com>
2025-10-28 18:49:35 +0530

    POS Ui Changed

--- app.py
+++ app.py
@@ -1704,6 +1704,6 @@ if __name__ == '__main__':
     #with app.app_context():
         #db.create_all()
         #create_default_admin()
-    app.run(debug=True) 
+    app.run(debug=True,) 
     #webview.create_window('Electrical Billing App', app,min_size=(700,500),frameless=False,resizable=True)
     #webview.start(ssl=True, http_server=True,debug=False)
